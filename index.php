db2e4420364149efbc9a70f5f2ad6bd11b80cfcc
Arshxll <70693770+Arshxll@users.noreply.github.com>
2020-10-02 15:45:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="em">
+<header>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-compatible" content="ie-edge">
+	<title> web </title>
+	<link rel="stylesheet" href="style.css">
+	<script src="https://kit.fontawesome.com/255ae60a1e.js" crossorigin="anonymous"></script>
+	<div class="section-header" > 
+	<h1> ARTIST-TA! </h1>
+	<div class="box1" > A </div>
+<div class="box2"> R </div>
+<div class="box3"> T </div>
+<div class="box4"> S </div>
+
+<div class="search-box">
+
+
+
+  <input class="search-txt" type="text" name="" placeholder="type to search"i class="fas fa-search">
+  <a class="search-btn" href="#"><i class="fas fa-search"></i> </a>
+
+</div>
+</header>
+<body>
+
+<content>
+<div class="bubble">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+     <div class="content1">
+     </div>
+</content>
+<div class="con-box"> BRUSHES </div>
+<div class="con-box1"> PAINTS </div>
+<div class="con-box2"> PENS </div>
+<div class="con-box3"> CANVAS </div>
+</body>
+
+<footer>
+
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-instagram"></a>
+<a href="#" class="fa fa-google"></a>
+
+
+		<p> copyright &copy; 2020   </p>
+
+	</footer>
+</html>
